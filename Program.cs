fc0da1ab7af0b1dfb12ecded62295779428247d1
Preethu P S <pspreethu05@gmail.com>
2024-11-15 17:30:31 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<101; i++)
+            {
+                if(i % 3 == 0 && i%5 == 0)
+                {
+                    Console.Write("fizzbuzz" + ",");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.Write("buzz" + ",");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.Write("fizz" + ",");
+                }
+                else
+                {
+                    Console.Write(i + ",");
+                }
+               
+            }
+
+        }
+    }
+}
